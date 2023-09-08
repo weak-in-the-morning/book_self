17a2682ce7e1736fef9d02f2fb8704bd343b84c9
Yuki Fujita <uni22yu@gmail.com>
2023-09-08 19:04:25 +0900

    add/bottom_barの作成と適応

---
+++ app/lib/view/components/bottom_bar.dart
@@ -0,0 +1,56 @@
+import 'package:app/data/colors.dart';
+import 'package:flutter/material.dart';
+
+class BottomAppBarComponent extends StatelessWidget {
+  const BottomAppBarComponent({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.home,
+            color: AppColors.ecruBeige,
+            size: 36,
+          ),
+          label: 'Bookshelf',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.add,
+            color: AppColors.ecruBeige,
+            size: 36,
+          ),
+          label: 'Register',
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.list,
+              color: AppColors.ecruBeige,
+              size: 36,
+            ),
+            label: 'Index'),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings,
+              color: AppColors.ecruBeige,
+              size: 36,
+            ),
+            label: 'Setting'),
+      ],
+      backgroundColor: AppColors.cobaltGreen,
+      type: BottomNavigationBarType.fixed,
+    );
+  }
+}
