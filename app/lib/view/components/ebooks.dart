1e4392f637d59cc999f97d3990ce46225c4714cc
izumiki <izumiki514@gmail.com>
2023-09-11 10:23:46 +0900

    update: 電子書籍サービスのラベル部分を削除

--- app/lib/view/components/ebooks.dart
+++ app/lib/view/components/ebooks.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:app/view/components/ebook_button.dart';
 
 class Ebooks extends StatelessWidget {
-  final String label;
+  // final String label;
   // final GestureTapCallback onPressed;
 
   const Ebooks({
     super.key, 
-    required this.label, 
+    // required this.label, 
     });
 
   @override
