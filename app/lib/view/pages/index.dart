bee81ce6f8a7813265fb4090552aa216dd0fe62d
Yuki Fujita <uni22yu@gmail.com>
2023-09-11 15:32:09 +0900

    add/本の一覧表示

--- app/lib/view/pages/index.dart
+++ app/lib/view/pages/index.dart
@@ -1,10 +1,112 @@
+import 'dart:math';
+
+import 'package:app/view/components/app_button.dart';
 import 'package:flutter/material.dart';
+
+//以下サンプル画像
+const sampleImgPath =
+    'https://asset.watch.impress.co.jp/img/gmw/docs/1510/331/1_l.jpg';
+
+const sampleLabel = 'ワンピース';
+
+const sampleImgPath2 = 'https://cdn.kdkw.jp/cover_500/199999/199999410133.jpg';
+
+const sampleLabel2 = 'ガンダム';
+
+const sampleImgPath3 =
+    'https://storage.mantan-web.jp/images/2020/11/16/20201116dog00m200027000c/001_size6.jpg';
+
+const sampleLabel3 = 'ラブライブ';
 
 class Index extends StatelessWidget {
   const Index({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('index');
+    return GridView.count(
+      crossAxisCount: 4,
+      children: [
+        // sample画像を繰り返し表示する3種類 * 50 = 150個描画されている
+        for (int i = 0; i < 50; i++) ...[
+          _book(
+            imagePath: sampleImgPath,
+            label: sampleLabel,
+          ),
+          _book(
+            imagePath: sampleImgPath2,
+            label: sampleLabel2,
+          ),
+          _book(
+            imagePath: sampleImgPath3,
+            label: sampleLabel3,
+          ),
+        ]
+      ],
+    );
+  }
+}
+
+class _book extends StatelessWidget {
+  const _book({super.key, required this.imagePath, required this.label});
+
+  final String imagePath;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    final imgWidth = MediaQuery.of(context).size.width / 4 - 64;
+    final dialogImageWidth = MediaQuery.of(context).size.width - 32;
+    return GestureDetector(
+      onTap: () => showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(
+                  16.0,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.network(
+                      imagePath,
+                      width: dialogImageWidth,
+                    ),
+                    Text(label),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AppButton.outlined(
+                          label: '閉じる',
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+      child: Column(
+        children: [
+          Transform.rotate(
+            angle: 10 * pi / 180,
+            child: Image.network(
+              imagePath,
+              width: imgWidth,
+              height: imgWidth * 2,
+            ),
+          ),
+          Text(
+            label,
+          )
+        ],
+      ),
+    );
   }
 }
