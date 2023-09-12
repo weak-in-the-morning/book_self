d9d5589fdb25ada7bba76b3eccf12bb9f5d1e357
keimo15 <keito.pokemon15@gmail.com>
2023-09-12 11:28:25 +0900

    fix: filename

--- app/lib/logic/README.md
+++ app/lib/logic/README.md
@@ -1,2 +1,28 @@
-# logic
-アプリが担うべき処理.今回はDB周りもここで良さそう(要議論)
+class BookData {
+  int id = 0;
+  String name = "";
+  int num = 0;
+  String service = "";
+  bool hasRead = false;
+  bool favorite = false;
+  String tag = "";
+  String memo = "";
+  DateTime created = DateTime.now();
+  DateTime updated = DateTime.now();
+  String urlSearch = "";
+  String urlImage = "";
+
+  BookData(
+      this.id,
+      this.name,
+      this.num,
+      this.service,
+      this.hasRead,
+      this.favorite,
+      this.tag,
+      this.memo,
+      this.created,
+      this.updated,
+      this.urlSearch,
+      this.urlImage);
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,67 +1,168 @@
-import 'package:app/view/design_system.dart';
+// main.dart
+import 'package:app/logic/database_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:app/data/book_data.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Book Self',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // DatabaseHelper クラスのインスタンス取得
+  final dbHelper = DatabaseHelper.instance;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  late BookData book = new BookData(
+    1,
+    'Book Title',
+    123,
+    'Service Name',
+    false,
+    false,
+    'Tag Name',
+    'Some memo text',
+    DateTime.now(),
+    DateTime.now(),
+    'Search URL',
+    'Image URL',
+  );
 
+  // homepage layout
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const AppBarComponent(title: 'BookSelef',),
+      appBar: AppBar(
+        title: Text('SQLiteデモ'),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: '名前',
+              ),
+              onChanged: (text) {
+                book.name = text;
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'サービス',
+              ),
+              onChanged: (text) {
+                book.service = text;
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: '巻数',
+              ),
+              onChanged: (text) {
+                book.num = int.parse(text);
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'URL',
+              ),
+              onChanged: (text) {
+                book.urlSearch = text;
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'タグ',
+              ),
+              onChanged: (text) {
+                book.tag = text;
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'メモ',
+              ),
+              onChanged: (text) {
+                book.memo = text;
+              },
+            ),
+            ElevatedButton(
+              child: Text(
+                '登録',
+                style: TextStyle(fontSize: 35),
+              ),
+              onPressed: _insert,
+            ),
+            ElevatedButton(
+              child: Text(
+                '照会',
+                style: TextStyle(fontSize: 35),
+              ),
+              onPressed: _query,
+            ),
+            ElevatedButton(
+              child: Text(
+                '更新',
+                style: TextStyle(fontSize: 35),
+              ),
+              onPressed: _update,
+            ),
+            ElevatedButton(
+              child: Text(
+                '削除',
+                style: TextStyle(fontSize: 35),
+              ),
+              onPressed: _delete,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+
+  // 登録ボタンクリック
+  void _insert() async {
+    final id = await dbHelper.insert(book);
+    print('登録しました。id: $id');
+  }
+
+  // 照会ボタンクリック
+  void _query() async {
+    final allRows = await dbHelper.queryAllRows();
+    print('全てのデータを照会しました。');
+    allRows.forEach(print);
+  }
+
+  // 更新ボタンクリック
+  void _update() async {
+    final rowsAffected = await dbHelper.update(book);
+    print('更新しました。 ID：$rowsAffected ');
+  }
+
+  // 削除ボタンクリック
+  void _delete() async {
+    final id = await dbHelper.queryRowCount();
+    final rowsDeleted = await dbHelper.delete(id!);
+    print('削除しました。 $rowsDeleted ID: $id');
   }
 }
