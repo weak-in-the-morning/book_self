1bda6bfb753160b25c6101f0bf6edb4008ff1cd7
seedri <serikawa.ed@gmail.com>
2023-09-12 18:01:32 +0900

    エラー直し
    ネタ切れた

--- app/lib/view/components/books_matrix.dart
+++ app/lib/view/components/books_matrix.dart
@@ -20,25 +20,25 @@ class BooksMatrix extends StatelessWidget {
             'https://image.mechacomi.jp/contents/002cwtgn.jpg',
           ]),
         ),
-        Padding(
+        const Padding(
             padding: EdgeInsets.only(top: gap),
-            child: const BooksRow(imagePaths: [
+            child: BooksRow(imagePaths: [
               'https://image.mechacomi.jp/contents/0027b2zi.jpg',
               'https://image.mechacomi.jp/contents/002ba6bb.jpg',
               'https://image.mechacomi.jp/contents/002f7nkl.jpg',
               'https://image.mechacomi.jp/contents/002hh21c.jpg',
             ])),
-        Padding(
+        const Padding(
             padding: EdgeInsets.only(top: gap),
-            child: const BooksRow(imagePaths: [
+            child: BooksRow(imagePaths: [
               'https://image.mechacomi.jp/contents/ee70128a-98c5-46dc-be2e-cb76d1738ff0.jpg',
               'https://image.mechacomi.jp/contents/e8b1e906-ad23-409f-ac48-c99d08b18bbf.jpg',
               'https://image.mechacomi.jp/contents/6a6c58aa-1d09-48af-adae-ebb0c6f42950.jpg',
               'https://image.mechacomi.jp/titles/538376.jpg',
             ])),
-        Padding(
+        const Padding(
             padding: EdgeInsets.only(top: gap),
-            child: const BooksRow(imagePaths: [
+            child: BooksRow(imagePaths: [
               'https://image.mechacomi.jp/contents/002kfyu2.jpg',
               'https://image.mechacomi.jp/contents/002kfyu4.jpg',
               'https://image.mechacomi.jp/contents/002kfyu6.jpg',
