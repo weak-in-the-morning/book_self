efe90b9c66a91a56f2f9c3d223e9644793574b56
seedri <serikawa.ed@gmail.com>
2023-09-12 10:39:43 +0900

    登録フォーム
    本の情報登録フォーム雛形
    電子書籍のとこは抜き

--- app/lib/view/pages/register.dart
+++ app/lib/view/pages/register.dart
@@ -1,27 +1,115 @@
 import 'package:flutter/material.dart';
 import 'package:app/view/components/app_button.dart';
 import 'package:app/view/components/input_field.dart';
+import 'package:app/view/components/ebooks.dart';
+import 'package:app/view/components/ebook_button.dart';
 
-class Register extends StatelessWidget {
-  const Register({
-    super.key,
-  });
-  // final TextEditingController controller2;
+class Register extends StatefulWidget {
+  @override
+  State<Register> createState() => _Register();
+}
+
+// enum BookStore{
+
+// }
+
+class _Register extends State<Register> {
+  int? selectedBookstoreIndex;
+  List<String> bookStores = ['まんがセゾン', 'Kindle', '楽天ブックス'];
+  List<String> bookStoreImages = [
+    'assets/images/Sezon.jpg',
+    'assets/images/Kindle.png',
+    'assets/images/RakutenBooks.png'
+  ];
+
+  // Widget button({required String text, required int index}) {
+  //   return InkWell(
+  //     splashColor: Colors.cyanAccent,
+  //     onTap: () {
+  //       setState(() {
+  //         selectedBookstoreIndex = index;
+  //       });
+  //     },
+  //     child: Container(
+  //       padding: const EdgeInsets.all(12),
+  //       color: index == selectedBookstoreIndex ? Colors.blue : Colors.white,
+  //       child: Text(
+  //         text,
+  //         style: TextStyle(
+  //           color:
+  //               index == selectedBookstoreIndex ? Colors.white : Colors.black,
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  // Widget button2({required int index, required String imgSrc}) {
+  //   return EbookButton(
+  //     label: bookStores[index],
+  //     imgSrc: bookStoreImages[index],
+  //     onPressed: () {
+  //       setState(() {
+  //         selectedBookstoreIndex = index;
+  //       });
+  //     },
+  //     child: Container(
+  //       padding: const EdgeInsets.all(12),
+  //       color: index == selectedBookstoreIndex ? Colors.blue : Colors.white,
+  //       child: Text(
+  //         text,
+  //         style: TextStyle(
+  //           color:
+  //               index == selectedBookstoreIndex ? Colors.white : Colors.black,
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController controller1 =
+    final TextEditingController controllerTitle =
         TextEditingController(text: '初期値');
-    return Column(
+    final TextEditingController controllerNum =
+        TextEditingController(text: '初期値');
+    final TextEditingController controllerURL =
+        TextEditingController(text: '初期値');
+    final TextEditingController controllerTag =
+        TextEditingController(text: '初期値');
+    final TextEditingController controllerMemo =
+        TextEditingController(text: '初期値');
+
+    //ボタンのウィジェット
+
+    return SingleChildScrollView(
+        child: Column(
       children: [
-        InputField(formTitle: '本のタイトル', controller: controller1),
+        InputField(formTitle: '本のタイトル', controller: controllerTitle),
+        InputField(formTitle: '巻数', controller: controllerNum),
+        // Row(
+        //   children: [
+        //     ...List.generate(
+        //       bookStores.length,
+        //       (index) => button(
+        //         index: index,
+        //         text: bookStores[index],
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        InputField(formTitle: 'URL', controller: controllerURL),
+        InputField(formTitle: 'タグ', controller: controllerTag),
+        InputField(formTitle: 'メモ', controller: controllerMemo),
         (AppButton.filled(
             label: '登録',
             onTap: () {
+              //ここに登録ボタン押した時の処理
+              //したはデバッグの時使ってた処理
               // String text = controller1.text;
               // debugPrint("入力内容は" + text);
             })),
       ],
-    );
+    ));
   }
 }
