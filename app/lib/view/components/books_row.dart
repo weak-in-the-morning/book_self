5b731b37058a1ac4240de359ba1eba15d42cbd1a
izumiki <izumiki514@gmail.com>
2023-09-12 12:04:35 +0900

    update: 本棚画像の縦横幅と間隙を動的に調整

--- app/lib/view/components/books_row.dart
+++ app/lib/view/components/books_row.dart
@@ -2,20 +2,20 @@ import 'package:app/view/components/book_in_bookshelf.dart';
 import 'package:flutter/material.dart';
 
 class BooksRow extends StatelessWidget {
-  final List<String> imgPaths;
+  final List<String> imagePaths;
   const BooksRow({
     super.key,
-    required this.imgPaths,
+    required this.imagePaths,
     });
 
   @override
   Widget build(BuildContext context) {
     return const Row(
       children: [
-        BookInBookshelf(imgPath: 'https://image.mechacomi.jp/contents/001fr2hw.jpg'),
-        BookInBookshelf(imgPath: 'https://image.mechacomi.jp/contents/001fr2hx.jpg'),
-        BookInBookshelf(imgPath: 'https://image.mechacomi.jp/contents/001fr2hy.jpg'),
-        BookInBookshelf(imgPath: 'https://image.mechacomi.jp/contents/001fr2hz.jpg'),
+        BookInBookshelf(imagePath: 'https://image.mechacomi.jp/contents/001fr2hw.jpg'),
+        BookInBookshelf(imagePath: 'https://image.mechacomi.jp/contents/001fr2hx.jpg'),
+        BookInBookshelf(imagePath: 'https://image.mechacomi.jp/contents/001fr2hy.jpg'),
+        BookInBookshelf(imagePath: 'https://image.mechacomi.jp/contents/001fr2hz.jpg'),
       ]
     );
   }
